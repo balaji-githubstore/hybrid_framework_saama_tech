572aae80d61396282a3a3805dcc519d13b4d21d4
balaji-githubstore <balaji0017@gmail.com>
2023-02-17 16:24:05 +0530

    Added Extent Report

--- src/test/java/com/saama/base/AutomationWrapper.java
+++ src/test/java/com/saama/base/AutomationWrapper.java
@@ -1,45 +1,79 @@
 package com.saama.base;
 
+import java.lang.reflect.Method;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class AutomationWrapper {
-	
+
 	protected WebDriver driver;
 
+	private ExtentReports extent;
+	protected ExtentTest test;
+
+	@BeforeSuite
+	public void init() {
+		extent = new ExtentReports();
+		ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
+		extent.attachReporter(spark);
+	}
+
+	@AfterSuite
+	public void end() {
+		extent.flush();
+	}
+
 	@BeforeMethod(alwaysRun = true)
-	@Parameters({"browser"})
-	public void setup(@Optional("ch") String browserName) 
-	{
-		if(browserName.equalsIgnoreCase("ff"))
-		{
-			driver=new FirefoxDriver();
-		}
-		else if (browserName.equalsIgnoreCase("edge"))
-		{
+	@Parameters({ "browser" })
+	public void setup(@Optional("edge") String browserName, Method method) {
+
+		test = extent.createTest(method.getName());
+
+		if (browserName.equalsIgnoreCase("ff")) {
+			driver = new FirefoxDriver();
+		} else if (browserName.equalsIgnoreCase("edge")) {
 			driver = new EdgeDriver();
-		}
-		else
-		{
-			System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+		} else {
+//			System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
 			driver = new ChromeDriver();
 		}
-		
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 	}
 
 	@AfterMethod(alwaysRun = true)
-	public void teardown() {
+	public void teardown(ITestResult result) {
+
+		if (result.getStatus() == ITestResult.FAILURE) {
+			test.log(Status.FAIL, MarkupHelper.createLabel(result.getName() + " FAILED ", ExtentColor.RED));
+			test.fail(result.getThrowable());
+		} else if (result.getStatus() == ITestResult.SUCCESS) {
+			test.log(Status.PASS, MarkupHelper.createLabel(result.getName() + " PASSED ", ExtentColor.GREEN));
+		} else {
+			test.log(Status.SKIP, MarkupHelper.createLabel(result.getName() + " SKIPPED ", ExtentColor.ORANGE));
+			test.skip(result.getThrowable());
+		}
+
 		driver.quit();
 	}
 
--- src/test/java/com/saama/test/LoginUITest.java
+++ src/test/java/com/saama/test/LoginUITest.java
@@ -4,22 +4,28 @@ import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.Status;
 import com.saama.base.AutomationWrapper;
+
 /**
  * 
  * @author Balaji Dinakaran
  *
- * Description - This class contains methods for validating UI components
+ *         Description - This class contains methods for validating UI
+ *         components
  */
 public class LoginUITest extends AutomationWrapper {
-	
-	@Test(priority = 1,groups = {"smoke","UI"})
+
+	@Test(priority = 1, groups = { "smoke", "UI" })
 	public void validateTitleTest() {
+		test.log(Status.INFO, "Validating Title Of the LoginPage");
 		String actualTitle = driver.getTitle();
+		test.log(Status.INFO, "Actual Title: " + actualTitle);
 		Assert.assertEquals(actualTitle, "OrangeHRM");
+		test.log(Status.INFO, "Successfully completed validation");
 	}
 
-	@Test(priority = 2,groups = {"UI"})
+	@Test(priority = 2, groups = { "UI" })
 	public void validateHeaderTest() {
 		String actualHeader = driver.findElement(By.xpath("//h5")).getText();
 		Assert.assertEquals(actualHeader, "Login");
